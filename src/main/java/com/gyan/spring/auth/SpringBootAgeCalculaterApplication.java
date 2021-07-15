3056c3b3582125ba93d9b7fd5bd494fa9fe96bb3
dhakadgyans <55667666+dhakadgyans@users.noreply.github.com>
2021-07-15 19:47:20 +0530

    Add files via upload

---
+++ src/main/java/com/gyan/spring/auth/SpringBootAgeCalculaterApplication.java
@@ -0,0 +1,13 @@
+package com.gyan.spring.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootAgeCalculaterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootAgeCalculaterApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/gyan/spring/auth/controller/AgeController.java
@@ -0,0 +1,25 @@
+package com.gyan.spring.auth.controller;
+
+import java.util.Date;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AgeController {
+
+	
+	@GetMapping("/birth-year/{year}")
+	public int ageFinder(@PathVariable int year) {
+		int finalage=0;
+		
+		Date date= new Date();
+		String s= date.toString();
+		String[] c=s.split(" ");
+		int sys=Integer.parseInt(c[5]);
+		finalage=sys-year;
+		return finalage;
+		
+	}
+}
